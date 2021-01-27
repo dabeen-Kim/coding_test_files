ef073b3b3d87358851532d84403409576f862caa
Dabeen Kim <69914290+dabeen-Kim@users.noreply.github.com>
2021-01-28 04:21:41 +0900

    implementation examples

--- implementation/imp_examples.py
+++ implementation/imp_examples.py
@@ -1,4 +1,4 @@
-#상하좌우 문제
+#'상하좌우' 문제
 N = int(input())
 plans = input().split()
 point = [1, 1]
@@ -22,7 +22,7 @@ print(point[0], point[1])
 
 --------------------------------------------------------------
 
-#시간 문제
+#'시간' 문제
 N = int(input())
 result = 0
 
@@ -34,3 +34,21 @@ for i in range(N+1):
 
 print(result)
 
+--------------------------------------------------------------
+
+#'왕실의 나이트' 문제
+point = input()
+columns = {'a':1, 'b':2, 'c':3, 'd':4, 'e':5, 'f':6, 'g':7, 'h':8}
+col , row = columns[point[0]], int(point[1])
+
+moves = [[-2,1], [-2,-1], [2,1], [2,-1], [1,2], [1,-2], [-1,2], [-1,-2]]
+count = 0
+
+for move in moves:
+    dcol = col + move[0]
+    drow = row + move[1]
+
+    if 0 < dcol < 9 and 0 < drow < 9:
+        count += 1
+
+print(count)
