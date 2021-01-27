952894ef51a71249fab438c88ac5d9227bd03f2f
Dabeen Kim <69914290+dabeen-Kim@users.noreply.github.com>
2021-01-27 23:22:59 +0900

    greedy easy examples

--- greedy/greedy_examples.py
+++ greedy/greedy_examples.py
@@ -1,4 +1,4 @@
-#큰 수의 법칙 문제
+#'큰 수의 법칙' 문제
 N, M, K = map(int, input().split())
 numlist = list(map(int, input().split()))
 
@@ -11,7 +11,7 @@ result = pattern*times + numlist[0]*remain
 
 print(result)
 
-#숫자 카드 게임 문제
+#'숫자 카드 게임' 문제
 N, M = map(int, input().split())
 cards = [[0]*M for _ in range(N)]
 result = []
@@ -21,3 +21,17 @@ for i in range(N):
     result.append(min(cards[i]))
 
 print(max(result))
+
+#'1이 될 때까지' 문제
+N, K = map(int, input().split())
+result = 0
+
+while N != 1:
+    if N % K == 0:
+        N //= K
+    else:
+        N -= 1
+    
+    result += 1
+
+print(result)
